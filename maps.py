4bebe64ae127a7dcf2ff26ce249c39d90c8a2896
addsch <addison148@gmail.com>
2022-10-28 22:45:38 -0700

    final commit

--- maps.py
+++ maps.py
@@ -1,38 +1,42 @@
-class maps:
-    dayDict = {
-        'Monday': 0,
-        'Tuesday': 0,
-        'Wednesday': 0,
-        'Thursday': 0,
-        'Friday': 0,
-        'Saturday': 0,
-        'Sunday': 0,
-    }
-    timeDict = {
-        '00': 0,
-        '01': 0,
-        '02': 0,
-        '03': 0,
-        '04': 0,
-        '05': 0,
-        '06': 0,
-        '07': 0,
-        '08': 0,
-        '09': 0,
-        '10': 0,
-        '11': 0,
-        '12': 0,
-        '13': 0,
-        '14': 0,
-        '15': 0,
-        '16': 0,
-        '17': 0,
-        '18': 0,
-        '19': 0,
-        '20': 0,
-        '21': 0,
-        '22': 0,
-        '23': 0,
-    }
-    topWords = {}    
-    wordCount = {}
+class maps(object):
+    # make sure day and time dictionaries are sorted when printing to ui
+    def __init__(self):
+        self.reset()
+    def reset(self):
+        self.dayDict = {
+            'Monday': 0,
+            'Tuesday': 0,
+            'Wednesday': 0,
+            'Thursday': 0,
+            'Friday': 0,
+            'Saturday': 0,
+            'Sunday': 0,
+        }
+        self.timeDict = {
+            '00': 0,
+            '01': 0,
+            '02': 0,
+            '03': 0,
+            '04': 0,
+            '05': 0,
+            '06': 0,
+            '07': 0,
+            '08': 0,
+            '09': 0,
+            '10': 0,
+            '11': 0,
+            '12': 0,
+            '13': 0,
+            '14': 0,
+            '15': 0,
+            '16': 0,
+            '17': 0,
+            '18': 0,
+            '19': 0,
+            '20': 0,
+            '21': 0,
+            '22': 0,
+            '23': 0,
+        }
+        self.topWords = {}    
+        self.wordCount = {}
